4191df056054ea98a398a3b202e6a88c6447887b
Heath Mayocchi <hmayocchi@gmail.com>
2018-06-15 23:39:43 +1000

    First Commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+Author: Smurfenstein
+-->
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>Home - Tv Tracker</title>
+		<!--
+		<script src="js/TTscript.js"></script>
+		<link href="css/TTstyle.css" rel="stylesheet" type="text/css" />
+		-->
+	</head>
+	<body>
+		<form action="php/searchBox.php">
+			<input type="text" name="searchText" placeholder="Search Torrents...">
+			<input type="submit" value="Search">
+		</form>
+	</body>
+</html>
---
+++ php/searchBox.php
@@ -0,0 +1,3 @@
+<?php
+echo file_get_contents("https://torrentz2.eu/search?f=star+wars+the+clone+wars");
+?>
