e8b165bfe0a36aa958b1a55331ef7cf196b89790
Heath Mayocchi <hmayocchi@gmail.com>
2018-06-17 06:32:49 +1000

    torrent regex

--- php/searchBox.php
+++ php/searchBox.php
@@ -5,6 +5,18 @@
 	regex results and magnet links
 	display list
 */
+	// assign search term
 	$searchTerm = $_POST["searchText"];
-	echo file_get_contents("https://thepiratebay.org/search/$searchTerm/0/99/0");
+	// get search result
+	$searchResult = file_get_contents("https://thepiratebay.org/search/$searchTerm/0/99/0");
+	// regex pattern for torrent results
+	$torrentPattern = "/(<td>
+<div class=\"detName\">			<a href=\"\/torrent)(...+)(center>
+		<\/td>)/su";
+	// apply regex pattern to results
+	preg_match($torrentPattern, $searchResult, $matches);
+	// add https://thepiratebay.org to the torrent and category links
+	// format list
+	
+	print_r($matches);
 ?>
