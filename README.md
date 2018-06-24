d5d1fc0e280e1f53e8a70797bdfbcb38897f7420
Heath Mayocchi <hmayocchi@gmail.com>
2018-06-24 20:08:11 +1000

    pirateiro regex

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Notify when new episode is available for download
 
 Display list of 10 results to choose from
 
-Include details for seeds/peers, uploader reputaion, file size
+Include details for seeds/peers, uploader reputation, file size
 
 Maybe use multiple torrent sites for results?
 
--- php/searchBox.php
+++ php/searchBox.php
@@ -1,36 +1,66 @@
 <?php
-
-/* 
-	search Piratebay
-	regex results and magnet links
-	display list
-*/
-	// assign posted search term
+	/* 
+	 *	assign posted search term
+	 */
+	
 	$searchTerm = $_POST["searchText"];
 	
-	// links for torrent site searches
+	/* 
+	 *	links for torrent site searches
+	 */
+	
 	$piratebay = "https://thepiratebay.org/search/$searchTerm/0/99/0";
+	$leetx = "https://1337x.to/search/$searchTerm/1/";
+	$pirateiro = "https://pirateiro.com/torrents/?search=$searchTerm&orderby=seeders";
 	
-	// add these sites later
-	//$leetx = "https://1337x.to/search/$searchTerm/1/";
-	//$pirateiro = "https://pirateiro.com/torrents/?search=$searchTerm";
+	/* 
+	 *	get search result
+	 */
 	
-	// get search result
-	$piratebayResult = file_get_contents($piratebay);
+	//$piratebayResult = file_get_contents($piratebay);
+	//$leetxResult = file_get_contents($leetx);
+	$pirateiroResult = file_get_contents($pirateiro);
+	//print_r($pirateiroResult);
 	
-	// regex pattern for torrent results
-	$piratebayPattern = "/(<td>
+	/* 
+	 *	regex patterns for torrent results
+	 */
+	
+/* 	$piratebayPattern = "/(<td>
 <div class=\"detName\">			<a href=\"\/torrent)(...+)(center>
-		<\/td>)/su";
+		<\/td>)/"; */
 		
-	// apply regex pattern to results
-	preg_match($piratebayPattern, $piratebayResult, $piratebayMatches);
+	$pirateiroPattern = "/(<table class=\"main\")(...+)(<br \/><br \/><br \/>)/s";
+		
+	/* 
+	 *	apply regex pattern to results
+	 */
 	
-	// remove array text
+	//preg_match($piratebayPattern, $piratebayResult, $piratebayMatches);	
+	preg_match($pirateiroPattern, $pirateiroResult, $pirateiroMatches);
 	
-	// add https://thepiratebay.org to the torrent, category and uploader links
+	/* 
+	 *	add torrent site domain to the page links
+	 */
 	
-	// format list
+	//torrent links
+	$pirateiroDomainPattern = "/(href=\"\/)/";
+	$pirateiroDomainReplacePattern = "href=\"https://pirateiro.com/";			
+	$pirateiroMatches = preg_replace($pirateiroDomainPattern, $pirateiroDomainReplacePattern, $pirateiroMatches);	
+	//uploader links
+	$pirateiroDomainPatternB = "/(href=\'\/)/";
+	$pirateiroDomainReplacePatternB = "href='https://pirateiro.com/";			
+	$pirateiroMatches = preg_replace($pirateiroDomainPatternB, $pirateiroDomainReplacePatternB, $pirateiroMatches);
+	//magnet links
+	$pirateiroMagnetPattern = "/( title=\"download this torrent using magnet link\">)/";
+	$pirateiroMagnetReplacePattern = "><b>M</b></a>";			
+	$pirateiroMatches = preg_replace($pirateiroMagnetPattern, $pirateiroMagnetReplacePattern, $pirateiroMatches);
 	
-	print_r($piratebayMatches);
+	/* 
+	 *	format list
+	 */
+	
+	//print_r($piratebayMatches);
+	//print_r($leetxMatches);
+	print_r($pirateiroMatches[0]);
 ?>
